7c69892b275b10af09aeca7e60ed647285c10da9
Saky <souhoc@gmail.com>
2022-04-08 19:13:13 +0200

    decookiise fixed

--- events/de_recookiise.js
+++ events/de_recookiise.js
@@ -1,16 +1,24 @@
 const valid_role_id = "960464388177940540";
-const target_id = "503513696790642689";
+const target_id = "474355372799557643";
 // const astrid = "290940926103257089";
 
 module.exports = {
 	name: "messageCreate",
 	async execute(msg) {
 		if (msg.author.bot) return;
-		if (msg.content !== "décookiisé" && msg.content !== "recookiisé") return;
+		if (
+			msg.content.toLowerCase() !== "décookiisé" &&
+			msg.content.toLowerCase() !== "recookiisé"
+		)
+			return;
 		if (!msg.member.roles.cache.map((role) => role.id).includes(valid_role_id))
 			return;
-		const member = await msg.guild.members.fetch(target_id);
-		await msg.channel.send(`${member} tu es ${msg.content}`);
+		try {
+			const member = await msg.guild.members.fetch(target_id);
+			await msg.channel.send(`${member} tu es ${msg.content}`);
+		} catch (err) {
+			console.error(err);
+		}
 		msg.delete();
 	},
 };
