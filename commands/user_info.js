9b868a36d8d220fdc94c41d817198e857f4cca4e
Saky <souhoc@gmail.com>
2022-04-08 11:57:31 +0200

    fix call api 42 more than 2 per sec

--- commands/user_info.js
+++ commands/user_info.js
@@ -5,7 +5,7 @@ const { MessageEmbed } = require("discord.js");
 const axios = require("axios");
 const { stripIndents } = require("common-tags");
 const moment = require("moment");
-const { getToken } = require("../utils.js");
+const { getToken, sleep } = require("../utils.js");
 
 module.exports = {
 	data: {
@@ -28,12 +28,13 @@ module.exports = {
 				Authorization: "Bearer " + token,
 			},
 		};
-		const coa = await getCoa(login, config);
+		const { color } = await getCoa(login, config);
+		const location = await getLocation(login, config);
 		axios
 			.get(uri + "users/" + login, config)
 			.then((res) => {
 				const user = res.data;
-				const embed = new MessageEmbed().setColor(coa.color);
+				const embed = new MessageEmbed().setColor(color);
 				const member = interaction.member;
 				embed
 					.setAuthor({
@@ -44,10 +45,6 @@ module.exports = {
 					.setTitle(`${user.login}'s infos`)
 					.setURL("https://profile.intra.42.fr/users/" + user.login)
 					.setThumbnail(user.image_url)
-					.setFooter({
-						text: `Asked by ${member.displayName}`,
-						iconURL: member.avatarURL(),
-					})
 					.setTimestamp()
 					.setDescription(user.displayname)
 					.addFields(
@@ -96,15 +93,17 @@ module.exports = {
 				if (user.groups.length) {
 					let groups = [];
 					user.groups.forEach((group) => groups.push(group.name.toLowerCase()));
-					embed.addField("groups", groups.join("\n"));
+					embed.addField("groups", groups.join("\n"), true);
 				}
 				if (user.titles.length) {
 					let titles = [];
 					user.titles.forEach((title) =>
 						titles.push(title.name.replace("%login", user.login))
 					);
-					embed.addField("titles", titles.join("\n"));
+					embed.addField("titles", titles.join("\n"), true);
 				}
+				if (location) embed.addField("Location", location, true);
+
 				interaction.reply({ embeds: [embed] });
 			})
 			.catch((err) => {
@@ -115,16 +114,30 @@ module.exports = {
 	},
 };
 
+// todo : 2 req API 42 / sec dépassés => go timout entre chaques
+
 function getPrimaryCampus(user) {
 	const primary = user.campus_users.filter((camp) => camp.is_primary)[0];
 	return user.campus.filter((camp) => camp.id === primary.campus_id)[0];
 }
 
-async function getCoa(user_id, config) {
+async function getCoa(user, config) {
 	return await axios
-		.get(uri + "users/" + user_id + "/coalitions", config)
+		.get(uri + "users/" + user + "/coalitions", config)
 		.then((res) => {
+			sleep(500);
 			return res.data.filter((coa) => coa.slug.includes("42cursus-paris"))[0];
+		})
+		.catch((err) => console.log(err));
+}
+
+async function getLocation(user, config) {
+	return await axios
+		.get(uri + "users/" + user + "/locations", config)
+		.then((res) => {
+			sleep(500);
+			if (!res.data[0].end_at) return res.data[0].host;
+			return null;
 		})
 		.catch((err) => console.log(err));
 }
