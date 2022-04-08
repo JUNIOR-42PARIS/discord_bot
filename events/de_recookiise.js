a891655228a86fbb1131eb44807c3ae39475a45e
Saky <souhoc@gmail.com>
2022-04-08 18:38:13 +0200

    troll romane cookiisé

--- events/de_recookiise.js
+++ events/de_recookiise.js
@@ -1,6 +1,6 @@
-const valid_role_id = "701034642127716424";
+const valid_role_id = "960464388177940540";
 const target_id = "503513696790642689";
-const astrid = "290940926103257089";
+// const astrid = "290940926103257089";
 
 module.exports = {
 	name: "messageCreate",
@@ -9,7 +9,7 @@ module.exports = {
 		if (msg.content !== "décookiisé" && msg.content !== "recookiisé") return;
 		if (!msg.member.roles.cache.map((role) => role.id).includes(valid_role_id))
 			return;
-		const member = await msg.guild.members.fetch("503513696790642689");
+		const member = await msg.guild.members.fetch(target_id);
 		await msg.channel.send(`${member} tu es ${msg.content}`);
 		msg.delete();
 	},
