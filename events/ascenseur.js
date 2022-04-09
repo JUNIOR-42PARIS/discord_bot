e19de32390f43f917260c745850d4c886f22ab44
Saky <souhoc@gmail.com>
2022-04-09 10:32:18 +0200

    astrid only cookie + ascenseur troll

---
+++ events/ascenseur.js
@@ -0,0 +1,41 @@
+const { stripIndents } = require("common-tags");
+const { sleep } = require("../utils.js");
+const tuteurs = "795296219370094602";
+
+module.exports = {
+	name: "messageCreate",
+	async execute(msg) {
+		if (msg.author.bot) return;
+		if (!msg.member.roles.cache.map((role) => role.id).includes(tuteurs))
+			return;
+		if (msg.content.toLowerCase() !== "ascenseur") return;
+
+		const ret = await msg.channel.send(stripIndents`
+		🛗 
+		⛓ 
+		⛓ 
+		⛓ 
+	`);
+		await sleep(300);
+		ret.edit(stripIndents`
+		⛓ 
+		🛗 
+		⛓ 
+		⛓ 
+	`);
+		await sleep(300);
+		ret.edit(stripIndents`
+		⛓ 
+		⛓ 
+		🛗 
+		⛓ 
+	`);
+		await sleep(300);
+		ret.edit(stripIndents`
+		⛓ 
+		⛓ 
+		⛓ 
+		🛗 
+	`);
+	},
+};
--- events/de_recookiise.js
+++ events/de_recookiise.js
@@ -1,24 +1,40 @@
-const valid_role_id = "960464388177940540";
+const cookie_role = "960464388177940540";
 const target_id = "474355372799557643";
-// const astrid = "290940926103257089";
+const astrid = "290940926103257089";
 
 module.exports = {
 	name: "messageCreate",
 	async execute(msg) {
 		if (msg.author.bot) return;
-		if (
-			msg.content.toLowerCase() !== "décookiisé" &&
-			msg.content.toLowerCase() !== "recookiisé"
-		)
-			return;
-		if (!msg.member.roles.cache.map((role) => role.id).includes(valid_role_id))
-			return;
-		try {
-			const member = await msg.guild.members.fetch(target_id);
-			await msg.channel.send(`${member} tu es ${msg.content}`);
-		} catch (err) {
-			console.error(err);
+		if (msg.author.id !== astrid) return;
+		switch (msg.content.toLowerCase()) {
+			case "décookiisé":
+				try {
+					const member = await msg.guild.members.fetch(target_id);
+					if (member.roles.cache.map((role) => role.id).includes(cookie_role))
+						return msg.reply("Elle est déjà décookiisée");
+					await msg.channel.send(
+						`${member} tu es ${msg.content.toLowerCase()}! 🍪`
+					);
+					await member.roles.add(cookie_role);
+				} catch (err) {
+					console.error(err);
+				}
+				break;
+			case "recookiisé":
+				try {
+					const member = await msg.guild.members.fetch(target_id);
+					if (!member.roles.cache.map((role) => role.id).includes(cookie_role))
+						return msg.reply("Elle a déjà un 🍪");
+					await msg.channel.send(
+						`${member} tu es ${msg.content.toLowerCase()}! 🍪`
+					);
+					await member.roles.remove(cookie_role);
+				} catch (err) {
+					console.error(err);
+				}
+
+				break;
 		}
-		msg.delete();
 	},
 };
