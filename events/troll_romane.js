6c2f4b59c701334bcec4a111185011b77036c2ae
Saky <souhoc@gmail.com>
2022-04-10 23:30:00 +0200

    secure some stuff and all you know

--- events/troll_romane.js
+++ events/troll_romane.js
@@ -3,10 +3,14 @@
 	Troll voté et voulu par tous les tuteurs.
 	<3 Romane. Cookie
 */
+
+const { restricted_guilds } = require("../config.json");
+
 module.exports = {
 	name: "messageCreate",
 	async execute(msg) {
 		if (msg.author.bot) return;
+		if (restricted_guilds.includes(msg.guild.id)) return;
 
 		try {
 			if (msg.author.id === "474355372799557643") await msg.react("🍪");
