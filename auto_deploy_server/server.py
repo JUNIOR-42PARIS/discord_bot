292cba3242454be0f70bfb149a709172b2bd5578
Damien Hubleur <hubleur.damien333@protonmail.ch>
2022-04-10 16:52:29 +0200

    repare python script

--- auto_deploy_server/server.py
+++ auto_deploy_server/server.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route("/deploy", methods=['POST'])
 def auto_deploy():
-    p = subprocess.Popen(['git','pull'], cwd='/home/saky/lld_bot_discord')
-    p.wait()
+	p = subprocess.Popen(['git','pull'], cwd='/home/saky/lld_bot_discord')
+	p.wait()
 	p = subprocess.Popen(['pm2','reload','bot_lld_sudo'], cwd='/home/saky')
-    p.wait()
-    print("")
+	p.wait()
+	print("")
 	print("Auto deployed !")
 	print("")
-    return "Deployd"
+	return "Deployd"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4242, debug=True, ssl_context="adhoc")
