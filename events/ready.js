5657c64bdb279a85daaed2b9a429df0532702579
Damien Hubleur <hubleur.damien333@protonmail.ch>
2022-04-10 21:12:26 +0200

    reset as stable state

--- events/ready.js
+++ events/ready.js
@@ -2,13 +2,6 @@ module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
-		client.user.setPresence({
-			status: 'online',
-			activity: {
-				name: "attendre la Saint-Patrâcques",
-				type: "PLAYING"
-			}
-		});
 		console.log(`${client.user.username}'s up!`);
 	},
 };
