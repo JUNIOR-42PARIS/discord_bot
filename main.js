4453d3ec80ce34b8a6898276b01d5307144f1002
Saky <souhoc@gmail.com>
2022-04-11 19:32:03 +0200

    intents members

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const fs = require("node:fs");
 const { Client, Collection, Intents } = require("discord.js");
 
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MEMBERS,
+	],
 });
 
 client.commands = new Collection();
