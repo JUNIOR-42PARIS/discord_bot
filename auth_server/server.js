30f97281bdc6b528a84b38a49b0cc07a905bc1f9
dhubleur <dhubleur@student.42.fr>
2022-04-14 11:48:41 +0000

    clean

--- auth_server/server.js
+++ auth_server/server.js
@@ -7,11 +7,6 @@ app.get('/auth', function (req, res) {
     res.redirect("https://api.intra.42.fr/oauth/authorize?client_id=85572e681d846e10b545098ab236aaa69d0b8c36cbc8b026a87e71d948045fe0&redirect_uri=https%3A%2F%2Fdamien-hubleur.tech%3A2424&response_type=code");
 });
 
-app.use(function(req, res, next) {
-    res.status(404).send("Y a rien a voir ici, désolé ;)");
-});
-
-// start the server in the port 3000 !
 app.listen(port, function () {
     console.log('App listening on port ' + port + '.');
 });
