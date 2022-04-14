656cb98c5f9af1b96928c79d15d6af32053068ee
dhubleur <dhubleur@student.42.fr>
2022-04-14 10:28:00 +0000

    web server

--- auth_server/server.js
+++ auth_server/server.js
@@ -1,1 +1,14 @@
-const http = require(" http");
+const http = require("http");
+
+const host = "0.0.0.0";
+const port = 2424;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("Hey welcome !");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on port ${port}`);
+});
