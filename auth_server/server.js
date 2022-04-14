8cb7c012553f4f1c7f1abe211d3001ff4c7c5932
dhubleur <dhubleur@student.42.fr>
2022-04-14 14:50:14 +0000

    clean and comment

--- auth_server/server.js
+++ auth_server/server.js
@@ -1,17 +1,13 @@
 require("dotenv").config();
 
 const fs = require('fs');
-const http = require('http');
 const https = require('https');
-
 const express = require('express');
-var app = express();
-
 const axios = require("axios");
 const { getToken, sleep } = require("../utils.js");
 
 const port = 2424;
-
+// SSL certificate server informations
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/damien-hubleur.tech/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/damien-hubleur.tech/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/damien-hubleur.tech/chain.pem', 'utf8');
@@ -21,10 +17,16 @@ const credentials = {
 	ca: ca
 };
 
+var app = express();
+
+// The endpoint for the auth. Need to pass a unique code with /auth?user=XXX
+// Use the XXX in the redirect uri
 app.get('/auth', function (req, res) {
     res.redirect("https://api.intra.42.fr/oauth/authorize?client_id=85572e681d846e10b545098ab236aaa69d0b8c36cbc8b026a87e71d948045fe0&redirect_uri=https%3A%2F%2Fdamien-hubleur.tech%3A2424%2F42result?user=XXX&response_type=code");
 });
 
+// Then endpoint for the result of the auth. We exchange the code with a token and with it, we can access to the user's information
+// We get the XXX from the URI to associate with a discord account
 app.get('/42result', function (req, res) {
     if(req.query.error)
         res.status(200).send("Désolé, nous n'avons pas pu t'identifier !");
@@ -51,8 +53,8 @@ app.get('/42result', function (req, res) {
     }
 });
 
+// Creation of the https server
 const httpsServer = https.createServer(credentials, app);
-
 httpsServer.listen(2424, () => {
 	console.log('Server running on port 2424');
 });
