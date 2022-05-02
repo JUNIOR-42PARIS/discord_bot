e2bd41255b9d81f18f7562b63ca196a13721e7d8
Saky <souhoc@gmail.com>
2022-05-02 16:55:31 +0200

    Will it work?

--- auth_server/server.js
+++ auth_server/server.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const https = require("https");
 
 const { readDB, writeDB } = require("../utils.js");
-const { guildId } = require("../config.json")
+const { guildId } = require("../config.json");
 
 var credentials;
 // SSL certificate server informations
@@ -31,8 +31,6 @@ try {
 } catch (err) {
 	console.error(err);
 }
-
-
 
 // Function that take the token and try to get user's informations from it
 async function getUserInformations(token, user_res, user_code, client) {
@@ -61,86 +59,92 @@ async function getUserInformations(token, user_res, user_code, client) {
 		});
 }
 
-async function validateAuth (discordUserId, user, client) {
-    //todo: const groups = user.groups,
-    //todo: Tuteur ? Staff ? Bocal ?
-    //todo: rename <name> (login)
+async function validateAuth(discordUserId, user, client) {
+	//todo: const groups = user.groups,
+	//todo: Tuteur ? Staff ? Bocal ?
+	//todo: rename <name> (login)
 
-    const guild = await client.guilds.fetch(guildId);
-    const member = await guild.members.fetch(discordUserId);
+	const guild = await client.guilds.fetch(guildId);
+	const member = await guild.members.fetch(discordUserId);
 
-    const bocal = user["staff?"];
+	const bocal = user["staff?"];
 	const tuteur = user.groups.find((g) => g.id == 40);
 
-    try {
-        await member.setNickname(`${user.first_name} (${user.login})`);
-        if (bocal) await member.roles.add("960464782132142151");
-        if (tuteur) await member.roles.add("960464388177940540");
-    } catch (err) {
-        console.error(err);
-    }
+	try {
+		await member.setNickname(`${user.first_name} (${user.login})`);
+		if (bocal) await member.roles.add("960464782132142151");
+		if (tuteur) await member.roles.add("960464388177940540");
+		await member.roles.add("954063445634985984");
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 function startApp(client) {
-    let app = express();
+	let app = express();
 
-    // The endpoint for the auth. Need to pass a unique code with /auth?user=XXX
-    // Use the XXX in the redirect uri
-    app.get("/auth", function (req, res) {
-        const db = readDB("./auth_server/users.json");
-        const user_code = req.query.user_code;
-        const found = db.some((o) => o.code === user_code);
-        if (!user_code || !found)
-            res
-                .status(400)
-                .send("Désolé, nous n'avons pas pu récupérer ton code unique !");
-        else
-            res.redirect(
-                "https://api.intra.42.fr/oauth/authorize?client_id=85572e681d846e10b545098ab236aaa69d0b8c36cbc8b026a87e71d948045fe0&redirect_uri=https%3A%2F%2Fdamien-hubleur.tech%3A2424%2F42result?user_code=" +
-                    user_code +
-                    "&response_type=code"
-            );
-    });
+	// The endpoint for the auth. Need to pass a unique code with /auth?user=XXX
+	// Use the XXX in the redirect uri
+	app.get("/auth", function (req, res) {
+		const db = readDB("./auth_server/users.json");
+		const user_code = req.query.user_code;
+		const found = db.some((o) => o.code === user_code);
+		if (!user_code || !found)
+			res
+				.status(400)
+				.send("Désolé, nous n'avons pas pu récupérer ton code unique !");
+		else
+			res.redirect(
+				"https://api.intra.42.fr/oauth/authorize?client_id=85572e681d846e10b545098ab236aaa69d0b8c36cbc8b026a87e71d948045fe0&redirect_uri=https%3A%2F%2Fdamien-hubleur.tech%3A2424%2F42result?user_code=" +
+					user_code +
+					"&response_type=code"
+			);
+	});
 
-    // Then endpoint for the result of the auth. We exchange the code with a token and with it, we can access to the user's information
-    // We get the XXX from the URI to associate with a discord account
-    app.get("/42result", function (req, user_res) {
-        if (req.query.error || !req.query.code || !req.query.user_code) {
-            console.error("Error occured during auth");
-            user_res.status(400).send("Désolé, nous n'avons pas pu t'identifier !");
-        } else {
-            const db = readDB("./auth_server/users.json");
-            const code = req.query.code;
-            const user_code = req.query.user_code;
-            const found = db.some((o) => o.code === user_code);
-            if (!found)
-                user_res
-                    .status(400)
-                    .send("Désolé, nous n'avons pas pu récupérer ton code unique !");
-            const params = {
-                grant_type: "authorization_code",
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET,
-                code: code,
-                redirect_uri:
-                    "https://damien-hubleur.tech:2424/42result?user_code=" + user_code,
-            };
-            axios
-                .post("https://api.intra.42.fr/oauth/token", params)
-                .then(async (res) => {
-                    await getUserInformations(res.data.access_token, user_res, user_code, client);
-                })
-                .catch((err) => {
-                    console.error("Impossible to transform user's code into token:");
-                    console.log(err);
-                    user_res
-                        .status(400)
-                        .send("Désolé, nous n'avons pas pu récupérer tes informations");
-                });
-        }
-    });
+	// Then endpoint for the result of the auth. We exchange the code with a token and with it, we can access to the user's information
+	// We get the XXX from the URI to associate with a discord account
+	app.get("/42result", function (req, user_res) {
+		if (req.query.error || !req.query.code || !req.query.user_code) {
+			console.error("Error occured during auth");
+			user_res.status(400).send("Désolé, nous n'avons pas pu t'identifier !");
+		} else {
+			const db = readDB("./auth_server/users.json");
+			const code = req.query.code;
+			const user_code = req.query.user_code;
+			const found = db.some((o) => o.code === user_code);
+			if (!found)
+				user_res
+					.status(400)
+					.send("Désolé, nous n'avons pas pu récupérer ton code unique !");
+			const params = {
+				grant_type: "authorization_code",
+				client_id: process.env.CLIENT_ID,
+				client_secret: process.env.CLIENT_SECRET,
+				code: code,
+				redirect_uri:
+					"https://damien-hubleur.tech:2424/42result?user_code=" + user_code,
+			};
+			axios
+				.post("https://api.intra.42.fr/oauth/token", params)
+				.then(async (res) => {
+					await getUserInformations(
+						res.data.access_token,
+						user_res,
+						user_code,
+						client
+					);
+				})
+				.catch((err) => {
+					console.error("Impossible to transform user's code into token:");
+					console.log(err);
+					user_res
+						.status(400)
+						.send("Désolé, nous n'avons pas pu récupérer tes informations");
+				});
+		}
+	});
 
-    const httpsServer = https.createServer(credentials, app);
-    return httpsServer;
+	const httpsServer = https.createServer(credentials, app);
+	return httpsServer;
 }
 module.exports = { startApp };
