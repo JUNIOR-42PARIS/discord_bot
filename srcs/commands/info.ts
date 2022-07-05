58c1161a3f696e57894611f52bb0fd4e4d9cde1b
DontBreakAlex <pro@alexandre-seo.tk>
2022-07-05 15:26:56 +0200

    Fix info

--- srcs/commands/info.ts
+++ srcs/commands/info.ts
@@ -7,7 +7,7 @@ const ownerGuildId = process.env.INFO_GUILDID;
 const memberRoles: Record<string, string> | undefined = (() => {
 	if (process.env.MEMBER_ROLES_JSON) {
 		try {
-			return JSON.parse(process.env.MEMBER_ROLES_JSON)
+			return JSON.parse(process.env.MEMBER_ROLES_JSON);
 		} catch {}
 	}
 })();
@@ -40,14 +40,14 @@ export default {
 			const client = interaction.client;
 			const guild = await client.guilds.fetch(ownerGuildId);
 			await guild.members.fetch();
-			let alreadyAdded: string[] = [];
+			const alreadyAdded: string[] = [];
 			for (const [roleId, roleName] of Object.entries(memberRoles)) {
 				const role = await guild.roles.fetch(roleId);
 				if (!role)
 					continue;
-				const members = Array.from(role.members.filter((_, k) => !alreadyAdded.includes(k)).keys());
-				alreadyAdded.push(...members);
-				data.addField(roleName, members.join('\n'), false);
+				const members = role.members.filter((_, k) => !alreadyAdded.includes(k));
+				alreadyAdded.push(...members.keys());
+				data.addField(roleName, [...members.values()].join('\n'), false);
 			}
 		}
 
